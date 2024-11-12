0be6dc989e890adb8402979cd61ea24e8b8cbb7d
nurai2month <nuraj4560@gmail.com>
2024-11-12 18:37:37 +0600

    end profile end the page contacts

--- apps/crm/admin.py
+++ apps/crm/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
-from apps.crm import  models
+from apps.crm import models
 # Register your models here.
+
+class TeamPeopleTabularInline(admin.TabularInline):
+    model = models.TeamPeople
+    extra = 2
 
 @admin.register(models.Todo)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'task', 'description')
     list_filter = ('id', 'task')
+
+@admin.register(models.Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('id', 'title')
+
+@admin.register(models.TeamPerson)
+class TeamPersonAdmin(admin.ModelAdmin):
+    list_display = ('id', )
+    list_filter = ('id', )
+    inlines = (TeamPeopleTabularInline, )
--- apps/crm/views1/grid_users_views.py
+++ apps/crm/views1/grid_users_views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from apps.users.models import User
+from apps.users.models import User, UserComment
+from apps.crm import models as crm_models
 
 def contact_grid(request):
     title = "Пользователи"
@@ -59,14 +62,44 @@ def delete_item(request, user_id):
     item.delete()
     return redirect('contacts_grid') 
 
+
+@login_required
 def contacts_profile(request, get_user_id):
     title = "Профиль"
     get_user = User.objects.get(id=get_user_id)
+    post_url = request.build_absolute_uri(reverse('contacts_profile', args=[get_user_id])) 
+    
+    user_teams = crm_models.TeamPeople.objects.filter(user=get_user)
+    team_members = None
 
+    if user_teams.exists():
+        team = user_teams.first().team 
+        team_members = crm_models.TeamPeople.objects.filter(team=team)
+
+    if request.method == 'POST':
+        if 'comments' in request.POST:
+            text = request.POST.get('text')
+            comment = UserComment(user=request.user, profile=get_user, text=text)
+            comment.save()
+            return redirect('contacts_profile', get_user_id)
+    
     return render(request, 'applications/contacts/profile.html', {
         'title': title,
         'get_user': get_user,
+        'post_url': post_url,
+        'team_members': team_members, 
     })
 
 def contact_list(request):
-    return render(request, 'applications/contacts/list.html', locals())
+    title = "Список пользователей"
+    contact_users = User.objects.all()
+
+    paginator = Paginator(contact_users, 20)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'applications/contacts/list.html', {
+        'title': title,
+        'contact_users': page_obj,  
+    })
